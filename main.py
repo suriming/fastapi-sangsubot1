5ba2bee58af33d642295b87b5dfb6670141e6f24
suriming <soolim520@gmail.com>
2022-02-24 19:23:45 +0900

    counter test

--- main.py
+++ main.py
@@ -12,3 +12,15 @@ async def read_root():
 async def read_classes(data = None):
     result = Komoran().pos("안녕하세요 안녕")
     return {"classes": result}
+
+hon_tokens = [word.rstrip('\n') for word in open('komoran_honorific_token.txt', 'r',encoding='utf-8')]
+
+@app.post("/predict", response_model=counterResponse)
+async def honorific_token_counter(request: counterRequest):
+    cnt = 0
+    for i in komoran.pos(request.text):
+        if str(i) in hon_tokens:
+            cnt += 1
+    return counterResponse(
+      cnt = cnt
+    )
